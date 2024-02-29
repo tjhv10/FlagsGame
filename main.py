d5448d59e99f5b8463a3facb4e4a1fc96c1e8386
tjhv10 <achiyabennatan@gmail.com>
2024-02-29 13:11:18 +0200

    ok

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
         if guessed_country.lower().strip() == 'exit':
             print(correct_country)
             break
-        guessed_country = find_most_similar_string(guessed_country,countries) 
+        guessed_country = find_most_similar_string(guessed_country,countries)
         print("you guessed: "+ guessed_country)
         image2_path = "flags/" + guessed_country
 
